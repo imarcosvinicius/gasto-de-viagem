1e9ac08868685459a2357791739c55c9275a8871
Marcos Vinicius Nunes <53801645+marcosviniciusz@users.noreply.github.com>
2021-11-10 06:07:22 -0300

    input validation

--- app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.gastodeviagem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.gastodeviagem.databinding.ActivityMainBinding
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -26,12 +28,37 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun calculate() {
+        if (validationOK()) {
 
-        val distance = binding.editDistanse.text.toString().toFloat()
-        val price = binding.editPrice.text.toString().toFloat()
-        val autonomy = binding.editAutonomy.text.toString().toFloat()
+            try {
+                val distance = binding.editDistanse.text.toString().toFloat()
+                val price = binding.editPrice.text.toString().toFloat()
+                val autonomy = binding.editAutonomy.text.toString().toFloat()
 
-        val totalValue = (distance * price) / autonomy
-        binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+                val totalValue = (distance * price) / autonomy
+                binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+
+            } catch (nfe: NumberFormatException) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.informar_valores_validos), Toast.LENGTH_LONG).show()
+            }
+
+        } else {
+            Toast.makeText(this, getString(R.string.preencha_todos_campos), Toast.LENGTH_LONG)
+                .show()
+        }
+
+
+    }
+
+    private fun validationOK(): Boolean {
+        return (binding.editDistanse.text.toString() != ""
+                && binding.editDistanse.text.toString() != "0"
+                && binding.editPrice.text.toString() != ""
+                && binding.editPrice.text.toString() != "0"
+                && binding.editAutonomy.text.toString() != ""
+                && binding.editAutonomy.text.toString() != "0")
+
     }
 }
